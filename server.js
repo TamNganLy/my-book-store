425bbf9b0457b031458d91f61e1653ba1e97ff76
TamNganLy <tamly1810@gmail.com>
2025-10-23 00:24:00 -0700

    complete API routes

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 // GET All books
 app.get("/books", async(req,res) => {
   try {
-    const result = await db.query("SELECT * FROM book;");
+    const result = await db.query("SELECT * FROM books;");
     res.json(result.rows);
   } catch (err) {
     console.error(err);
@@ -34,10 +34,10 @@ app.get("/books", async(req,res) => {
 
 
 // GET book by isbn
-app.get("/books/isbn/:id", async(req,res) => {
+app.get("/books/isbn/:isbn", async(req,res) => {
   try {
-    const id = parseInt(req.params.id);
-    const result = await db.query("SELECT * FROM book WHERE id = $1;", [id]);
+    const isbn = req.params.isbn;
+    const result = await db.query("SELECT * FROM books WHERE id = $1;", [isbn]);
 
     if (result.rows.length === 0) {
       return res.status(404).send("Book not found.");
@@ -54,7 +54,7 @@ app.get("/books/isbn/:id", async(req,res) => {
 app.get("/books/genre", async(req,res) => {
   try {
     const genre = req.query.genre;
-    const result = await db.query("SELECT * FROM book WHERE genre = $1;", [genre]);
+    const result = await db.query("SELECT * FROM books WHERE genre = $1;", [genre]);
 
     if (result.rows.length === 0) {
       return res.status(404).send("Book not found.");
@@ -71,7 +71,7 @@ app.get("/books/genre", async(req,res) => {
 app.get("/books/search", async(req,res) => {
   try {
     const title = req.query.search.toLowerCase();
-    const result = await db.query("SELECT * FROM book WHERE LOWER(title) LIKE '%' || $1 || '%';", [title]);
+    const result = await db.query("SELECT * FROM books WHERE LOWER(title) LIKE '%' || $1 || '%';", [title]);
 
     if (result.rows.length === 0) {
       return res.status(404).send("Book not found.");
@@ -85,25 +85,140 @@ app.get("/books/search", async(req,res) => {
 })
 
 // GET review by book id
-app.get("/review/:id", async(req,res) => {
-  
+app.get("/review/:isbn", async(req,res) => {
+  try {
+    const isbn = req.params.isbn;
+    const result = await db.query("SELECT * FROM reviews WHERE book_id = $1;", [isbn]);
+
+    if (result.rows.length === 0) {
+      return res.status(404).send("There is no review for this book. Please add one.");
+    }
+
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server error");
+  }
 })
 
 // POST a book
 app.post("/books", async(req,res) => {
-  
+  const id = req.body.id;
+  const title = req.body.title;
+  const description = req.body.description;
+  const rate = req.body.rate;
+  const genre = req.body.genre;
+
+  try {
+    const result = await db.query(
+      "INSERT INTO books (id, title, description, rate, genre) VALUES ($1, $2, $3, $4, $5) RETURNING *;",
+      [id, title, description, rate, genre]
+    );
+
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server error");
+  }
+});
+
+// POST a review
+app.post("/review", async(req,res) => {
+  const book_id = req.body.isbn;
+  const review = req.body.review;
+
+  try {
+    const result = await db.query(
+      "INSERT INTO reviews (book_id, text) VALUES ($1, $2) RETURNING *;",
+      [book_id, review]
+    );
+
+    res.json(result.rows[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server error");
+  }
 })
 
 // UPDATE a book (one parameter)
-app.patch("/books/:id", async(req,res) => {
-  
+app.patch("/books/:isbn", async(req,res) => {
+  const isbn = req.params.isbn;
+  const title = req.body.updatedTitle || null;
+  const description = req.body.updatedDescription || null;
+  const rate = req.body.updatedRate !== undefined && req.body.updatedRate !== "" ? 
+    parseInt(req.body.updatedRate) : null;
+  const genre = req.body.updatedGenre || null;
+
+  try {
+    const result = await db.query(
+      `UPDATE books
+       SET 
+         title = COALESCE($1, title),
+         description = COALESCE($2, description),
+         rate = COALESCE($3, rate),
+         genre = COALESCE($4, genre)
+       WHERE id = $5
+       RETURNING *;`,
+      [title, description, rate, genre, isbn]
+    );
+
+    if (result.rows.length === 0) {
+      return res.status(404).send("Book not found.");
+    }
+
+    res.json(result.rows[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server error");
+  }
+})
+
+// UPDATE a review (one parameter)
+app.patch("/reviews/:id", async(req,res) => {
+  const id = parseInt(req.params.id);
+  const text = req.body.updatedReview;
+
+  if (!text) {
+    return res.status(400).send("No review text provided to update");
+  }
+
+  try {
+    const result = await db.query(
+      `UPDATE reviews SET text = $1 WHERE id = $2 RETURNING *;`,
+      [text, id]
+    );
+
+      res.json(result.rows[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server error");
+  }
 })
 
 // DELETE a book
-app.delete("/books/:id", async(req,res) => {
-  
+app.delete("/books/:isbn", async(req,res) => {
+  const deletedId = req.params.isbn;
+
+  try {
+    await db.query("DELETE FROM reviews WHERE book_id = $1", [deletedId]);
+    await db.query("DELETE FROM books WHERE id = $1", [deletedId]);
+    res.status(200).send("Book deleted.");
+  } catch (err) {
+    res.status(500).send("Server error");
+  }
 })
 
+// DELETE a review
+app.delete("/reviews/:id", async(req,res) => {
+  const deletedId = parseInt(req.params.id);
+
+  try {
+    await db.query("DELETE FROM reviews WHERE id = $1", [deletedId]);
+    res.status(200).send("Review deleted.");
+  } catch (err) {
+    res.status(500).send("Server error");
+  }
+})
 
 
 app.listen(port, () => {
---
+++ sql.sql
@@ -0,0 +1,60 @@
+CREATE TABLE reviews
+(
+  id integer PRIMARY KEY,
+  book_id VARCHAR(13) REFERENCES books (id),
+  text text
+)
+
+CREATE TABLE books
+(
+  id VARCHAR(13) NOT NULL PRIMARY KEY,
+  title character varying(100) NOT NULL,
+  description text,
+  rate integer NOT NULL,
+  genre character varying(25)
+)
+
+INSERT INTO review (book_id, text) 
+VALUES (
+	2, 
+	`Loved it! Great pacing and characters`
+)
+
+INSERT INTO review (book_id, text) 
+VALUES (
+	1, 
+	`Beautifully written and full of emotion. The world-building feels real and alive — definitely a must-read.`
+)
+
+INSERT INTO review (book_id, text) 
+VALUES (
+  1, 
+  `An enjoyable read with engaging characters and a fast-paced story. I couldn't put it down once I started!`
+  )
+
+INSERT INTO book (title, description, rate, isbn, genre) 
+VALUES (
+  'Flyte (Septimus Heap, Book 2)', 
+  `It has been a year since Septimus Heap discovered his real family and true calling to be a wizard. As Apprentice to Extra Ordinary Wizard Marcia Overstrand, he is learning the fine arts of Conjurations, Charms, and other Magyk, while Jenna is adapting to life as the Princess and enjoying the freedom of the Castle.But there is something sinister at work. Marcia is constantly trailed by a menacing Darke Shadow, and Septimus\'s brother Simon seems bent on a revenge no one understands. Why is the Darke Magyk still lingering?Bringing fantasy to new heights, Angie Sage continues the journey of Septimus Heap with her trademark humor and all of the clever details readers have come to love.`,
+  '4',
+  '0060577363',
+  'Fantasy'
+)
+
+INSERT INTO book (title, description, rate, isbn, genre) 
+VALUES (
+  'Magyk (Septimus Heap)', 
+  `Septimus Heap, the seventh son of the seventh son, disappears the night he is born, pronounced dead by the midwife. That same night, the baby's father, Silas Heap, comes across an abandoned child in the snow -- a newborn girl with violet eyes. The Heaps take her into their home, name her Jenna, and raise her as their own. But who is this mysterious baby girl, and what really happened to their beloved son Septimus?`,
+  '4',
+  '0060577320',
+  'Fantasy'
+)
+
+INSERT INTO book (title, description, rate, isbn, genre) 
+VALUES (
+  'Physik (Septimus Heap)', 
+  `When Silas Heap unSeals a forgotten room in the Palace, he releases the ghost of a Queen who lived five hundred years earlier. Queen Etheldredda is as awful in death as she was in life, and she's still up to no good. Her diabolical plan to give herself everlasting life requires Jenna's compliance, Septimus's disappearance, and the talents of her son, Marcellus Pye, a famous Alchemist and Physician. And if Queen Etheldredda's plot involves Jenna and Septimus, then it will surely involve Nicko, Alther Mella, Marcia Overstrand, Beetle, Stanley, Sarah, Silas, Spit Fyre, Aunt Zelda, and all of the other wacky, wonderful characters that made Magyk and Flyte so memorable`,
+  '3',
+  '006057738X',
+  'Fantasy'
+)
