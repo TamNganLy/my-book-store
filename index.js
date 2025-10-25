1cb9bf1093070d0d84d3c5bede5a78b6b59b212c
TamNganLy <tamly1810@gmail.com>
2025-10-25 00:22:32 -0700

    complete display book detail and its reviews

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import axios from 'axios';
+import express from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
 
 const app = express();
 const port = 3000;
@@ -10,16 +10,52 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static("public"));
 
-app.get("/", async(req, res) => {
+app.get("/", async (req, res) => {
   try {
     const response = await axios.get(`${API_URL}/books`);
     // console.log(response.data);
-    res.render("index.ejs", {books: response.data})
-  } catch(err) {
-    
+    res.render("index.ejs", {
+      nav: "main",
+      books: response.data,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server error");
   }
 });
 
+app.get("/isbn/:isbn", async (req, res) => {
+  const isbn = req.params.isbn;
+
+  try {
+    const book_response = await axios.get(`${API_URL}/books/${isbn}`);
+    const book = book_response.data[0];
+
+    let reviews = [];
+    let reviewMessage = null;
+    
+    try {
+      const review_response = await axios.get(`${API_URL}/reviews/${isbn}`);
+      reviews = review_response.data;
+    } catch (err) {
+      if (err.response && err.response.status === 404) {
+        reviewMessage = err.response.data;
+      } else {
+        throw err;
+      }
+    }
+    
+    res.render("index.ejs", {
+      nav: "book",
+      book,
+      reviews,
+      reviewMessage
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server error");
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,18 @@
-window.addEventListener("scroll", function () {
-  const navbar = document.querySelector(".custom-navbar");
-  if (window.scrollY > 0) {
-    navbar.classList.add("scrolled");
-    navbar.classList.remove("navbar-dark", "bg-dark");
-  } else {
-    navbar.classList.remove("scrolled");
-    navbar.classList.add("navbar-dark", "bg-dark");
-  }
-});
+const navbar = document.querySelector(".custom-navbar");
+
+if (currentPage === "main") {
+  // normal scroll behavior
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 0) {
+      navbar.classList.add("scrolled");
+      navbar.classList.remove("navbar-dark", "bg-dark");
+    } else {
+      navbar.classList.remove("scrolled");
+      navbar.classList.add("navbar-dark", "bg-dark");
+    }
+  });
+} else if (currentPage === "book") {
+  // always scrolled
+  navbar.classList.add("scrolled");
+  navbar.classList.remove("navbar-dark", "bg-dark");
+}
